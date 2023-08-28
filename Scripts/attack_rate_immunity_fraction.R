2952f1e437dd5534c7e2898ddd82c14ee241a31f
rafalopespx <lopes1313@gmail.com>
2023-08-28 00:49:31 -0400

    bivariate plots to estimates of immmunity

--- Scripts/attack_rate_immunity_fraction.R
+++ Scripts/attack_rate_immunity_fraction.R
@@ -38,14 +38,11 @@ states_attack_rate_immune<-left_join(states_fulldata,
   mutate(epiweek = end.of.epiweek(days)) 
 
 states_sum <- states_attack_rate_immune|> 
-  reframe(infections = sum(infections, na.rm = T),
-          pop = first(pop), 
-          values = sum(values, na.rm = T), 
-          .by = c("name_states", "variant", "type_protected")) |> 
-  mutate(incidence = (infections/pop)*10e5,
-         attack_rate = round((incidence/pop)*100, 2),
-         values = round(values, 2)) |> 
-  filter(!is.na(type_protected))
+  reframe(values = round(sum(values, na.rm = T), 2), 
+          .by = c("name_states", "variant", "type_protected")) |>
+  filter(!is.na(type_protected)) |> 
+  right_join(states_attack_rates) |> 
+  mutate(attack_rate = round((total_infections/pop)*100, 2))
 
 states_abb<-vroom("Data/states_abbrev_region_division.csv") |> 
   rename(name_states = `State`)
@@ -112,12 +109,13 @@ for(i in 1:length(protected_types)){
                       dim = 6,
                       xlab = "Higher (%) Effectively Protected",
                       ylab = "Higher (%) Attack Rate",
-                      size = 12)
+                      size = 12)+
+    theme(axis.text.x = element_text(angle = 90))
   
   # combine map with legend
   plot_list[[i]] <- ggdraw() +
     draw_plot(map, 0, 0, 1, 1) +
-    draw_plot(legend, .60, .10, 0.35, 0.35)
+    draw_plot(legend, .70, .10, 0.35, 0.35)
   # finalPlot
 }
 
@@ -126,6 +124,62 @@ plot_list[[2]]
 plot_list[[3]]
 plot_list[[4]]
 
-
-
-
+# variants<-unique(states_immune_map$variant)
+# 
+# plot_list2<-list()
+# 
+# for(i in 1:length(variants)){
+#   ## Data filtering
+#   data<-states_immune_map |> 
+#     filter(variant == variants[i])
+#   
+#   map<- data |> 
+#     ggplot()+
+#     geom_sf(aes(fill = bi_class),
+#             color = "grey50",
+#             size = 0.1, 
+#             show.legend = FALSE)+
+#     bi_scale_fill(pal = custom_pal6, 
+#                   dim = 6) +
+#     # bi_scale_color(pal = custom_pal6, 
+#     #                dim = 6) +
+#     labs(
+#       title = "Attack Rate vs. Effectively Protected",
+#       subtitle = variants[i]
+#     ) +
+#     theme_minimal()+
+#     facet_wrap(type_protected~., nrow = 1)
+#   map
+#   
+#   ## Labels
+#   labels1 <- bi_class_breaks(data, 
+#                              x = values, 
+#                              y = attack_rate, 
+#                              style = "quantile", 
+#                              dim = 6, 
+#                              dig_lab = 2, 
+#                              split = FALSE)
+#   ## Legend
+#   legend <- bi_legend(pal = custom_pal6,
+#                       breaks = labels1,
+#                       dim = 6,
+#                       xlab = "Higher (%) Effectively Protected",
+#                       ylab = "Higher (%) Attack Rate",
+#                       size = 12)+
+#     theme(axis.text.x = element_text(angle = 90))
+#   
+#   # combine map with legend
+#   plot_list2[[i]] <- ggdraw() +
+#     draw_plot(map, 0, 0, 1, 1) +
+#     draw_plot(legend, .1, .1, .35, 35)
+#   # finalPlot
+# }
+# 
+# plot_list2[[1]]
+# plot_list2[[2]]
+# plot_list2[[3]]
+# plot_list2[[4]]
+# 
+# 
+# 
+# 
